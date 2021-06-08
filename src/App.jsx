8f8857b995fc95f2974a0893c8fde7d8ae98cc01
Rostyk Lukavyi <rostyk.lukavyi842@gmail.com>
2021-06-09 01:27:25 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {AppWrapper} from "./styled/appStyled";
+import SideBar from "./components/Sidebar/sidebarStyled";
+
+function App() {
+  return (
+	  <AppWrapper>
+		<SideBar/>
+		{/* Right chat */}
+	  </AppWrapper>
+  );
+}
+
+export default App;
